ab3eda34c11198f937f6ae4deb0a83db23f8b8b3
Abyan Herfanto <abyanhrf@gmail.com>
2025-09-26 20:12:17 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -22,26 +22,21 @@ function validateForm() {
         return;
     }
 
-    // Validasi nama (hanya huruf dan spasi)
     let nameRegex = /^[A-Za-z\s]+$/;
     if (!nameRegex.test(name)) {
         alert("Nama tidak bisa diisi oleh angka dan simbol.");
         return;
     }
 
-    // Validasi email
     let emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
         alert("Silakan masukkan alamat email yang benar.");
         return;
     }
 
-    // Kalau semua valid
     alert(`Terima kasih, ${name}! Formulir berhasil dikirim!`);
 
-    // Reset form setelah submit
     document.getElementById("name").value = "";
     document.getElementById("email").value = "";
     document.getElementById("message").value = "";
-
 }
